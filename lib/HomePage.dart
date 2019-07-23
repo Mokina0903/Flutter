7d0a63f853928f1ac2783649ef08662a49087af7
monika.wiszmann <monika.wiszmann@gmail.com>
2019-07-23 15:27:11 +0200

    Add gui

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -13,6 +13,23 @@ class _HomePageState extends State<HomePage>{
 
   String message;
   List<String> gameState;
+  bool isCross = false;
+
+  void playGame(int index) {
+    if (this.gameState[index] == "empty") {
+      setState(() {
+        this.isCross
+            ? this.gameState[index] = "cross"
+            : this.gameState[index] = "circle";
+      });
+      this.isCross = ! this.isCross;
+      this.checkWin();
+    }
+  }
+
+  void checkWin() {
+
+  }
 
   @override
   void initState() {
@@ -38,9 +55,89 @@ class _HomePageState extends State<HomePage>{
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  AssetImage getImage(String value) {
+    switch (value) {
+      case ('cross'):
+        return cross;
+        break;
+      case ('circle'):
+        return circle;
+        break;
+      default:
+        return edit;
+    }
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("Tic Tac Toe", style: TextStyle(
+          color: Colors.white,
+        ),),
+        backgroundColor: Color(0xFF192A56),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(20.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0),
+              itemCount: this.gameState.length,
+              itemBuilder: (context, i) => SizedBox(
+                width: 100.0,
+                height: 100.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    this.playGame(i);
+                  },
+                  child: Image(
+                    image: this.getImage(this.gameState[i]),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              this.message,
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(32.0),
+            child: MaterialButton(
+              padding: EdgeInsets.all(20.0),
+              color: Color(0xFF0A3D62),
+              minWidth: 300.0,
+              height: 50.0,
+              child: Text(
+                "Reset Game",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                this.resetGame();
+              },
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
