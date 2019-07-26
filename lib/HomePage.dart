723a5d705b88be5287aad9fe0166f9dcc4285935
monika.wiszmann <monika.wiszmann@gmail.com>
2019-07-26 10:58:55 +0200

    Refactor gameState from list into 2d list

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -3,56 +3,54 @@ import 'package:flutter/material.dart';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => new _HomePageState();
-
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   AssetImage cross = AssetImage("images/cross.png");
   AssetImage circle = AssetImage("images/circle.png");
-  AssetImage edit = AssetImage("images/edit.png");
 
   String message;
-  List<String> gameState;
   bool isCross = false;
-
-  void playGame(int index) {
-    if (this.gameState[index] == "empty") {
-      setState(() {
-        this.isCross
-            ? this.gameState[index] = "cross"
-            : this.gameState[index] = "circle";
-      });
-      this.isCross = ! this.isCross;
-      this.checkWin();
-    }
-  }
-
-  void checkWin() {
-
-  }
+  List<List<String>> gameState;
 
   @override
   void initState() {
     super.initState();
     setState(() {
       this.gameState = [
-        "empty", "empty", "empty",
-        "empty", "empty", "empty",
-        "empty", "empty", "empty",
+        ["empty", "empty", "empty"],
+        ["empty", "empty", "empty"],
+        ["empty", "empty", "empty"],
       ];
       this.message = "";
     });
   }
 
-  void resetGame() {
+  void playGame(int x, int y) {
+    if (this.gameState[x][y] == "empty") {
+      setState(() {
+        this.isCross
+            ? this.gameState[x][y] = "cross"
+            : this.gameState[x][y] = "circle";
+      });
+      this.isCross = !this.isCross;
+      this._checkWin(x, y);
+    }
+  }
+
+  void _resetGame() {
     setState(() {
       this.gameState = [
-        "empty", "empty", "empty",
-        "empty", "empty", "empty",
-        "empty", "empty", "empty",
+        ["empty", "empty", "empty"],
+        ["empty", "empty", "empty"],
+        ["empty", "empty", "empty"],
       ];
       this.message = "";
     });
+  }
+
+  void _checkWin(int x, int y) {
+    //todo implement
   }
 
   AssetImage getImage(String value) {
@@ -64,7 +62,7 @@ class _HomePageState extends State<HomePage>{
         return circle;
         break;
       default:
-        return edit;
+        return null;
     }
   }
 
@@ -72,9 +70,12 @@ class _HomePageState extends State<HomePage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        title: Text("Tic Tac Toe", style: TextStyle(
-          color: Colors.white,
-        ),),
+        title: Text(
+          "Tic Tac Toe",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
         backgroundColor: Color(0xFF192A56),
       ),
       body: Column(
@@ -89,19 +90,8 @@ class _HomePageState extends State<HomePage>{
                   childAspectRatio: 1.0,
                   crossAxisSpacing: 10.0,
                   mainAxisSpacing: 10.0),
-              itemCount: this.gameState.length,
-              itemBuilder: (context, i) => SizedBox(
-                width: 100.0,
-                height: 100.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    this.playGame(i);
-                  },
-                  child: Image(
-                    image: this.getImage(this.gameState[i]),
-                  ),
-                ),
-              ),
+              itemCount: this.gameState.length * this.gameState.length,
+              itemBuilder: _buildGridRow,
             ),
           ),
           Container(
@@ -129,7 +119,7 @@ class _HomePageState extends State<HomePage>{
                 ),
               ),
               onPressed: () {
-                this.resetGame();
+                this._resetGame();
               },
               shape: ContinuousRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
@@ -137,6 +127,39 @@ class _HomePageState extends State<HomePage>{
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Widget _buildGridRow(BuildContext context, int index) {
+    int gridStateLength = gameState.length;
+    int x, y = 0;
+    x = (index / gridStateLength).floor();
+    y = (index % gridStateLength);
+    return GestureDetector(
+        child: Container(
+          child: Center(
+            child: _buildGridCell(x, y),
+          ),
+        ),
+    );
+  }
+
+
+  Widget _buildGridCell(int x, int y) {
+    return  SizedBox(
+      width: 100.0,
+      height: 100.0,
+      child: MaterialButton(
+        color: Colors.indigoAccent[100],
+        onPressed: () {
+          this.playGame(x, y);
+        },
+        child: this.getImage(this.gameState[x][y]) == null
+            ? null
+            : Image(
+          image: this.getImage(this.gameState[x][y]),
+        ),
       ),
     );
   }
